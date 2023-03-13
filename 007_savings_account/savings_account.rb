be68cb2d4b8de033e977cbe9c51cef56b93354fe
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-03-13 18:01:43 +0100

    refactor: define class methods in class << self block

--- 007_savings_account/savings_account.rb
+++ 007_savings_account/savings_account.rb
@@ -1,24 +1,26 @@
 module SavingsAccount
-  def self.interest_rate(balance)
-    return 3.213 if balance.negative?
+  class << self
+    def interest_rate(balance)
+      return 3.213 if balance.negative?
 
-    return 0.5 if balance < 1000
+      return 0.5 if balance < 1000
 
-    return 1.621 if balance < 5000
+      return 1.621 if balance < 5000
 
-    2.475
-  end
-
-  def self.annual_balance_update(balance)
-    balance + balance * interest_rate(balance) * 0.01
-  end
-
-  def self.years_before_desired_balance(current_balance, desired_balance)
-    years = 0
-    while current_balance <= desired_balance
-      current_balance = annual_balance_update(current_balance)
-      years += 1
+      2.475
     end
-    years
+
+    def self.annual_balance_update(balance)
+      balance + balance * interest_rate(balance) * 0.01
+    end
+
+    def self.years_before_desired_balance(current_balance, desired_balance)
+      years = 0
+      while current_balance <= desired_balance
+        current_balance = annual_balance_update(current_balance)
+        years += 1
+      end
+      years
+    end
   end
 end
