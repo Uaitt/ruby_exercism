46bfa93caa9fbad583c4b891d217796b5eb4b23a
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-03-14 12:25:07 +0100

    refactor: use class << self block

--- 011_locomotive_engineer/locomotive_engineer.rb
+++ 011_locomotive_engineer/locomotive_engineer.rb
@@ -1,18 +1,20 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons(*wagon_ids)
-    wagon_ids
-  end
+  class << self
+    def generate_list_of_wagons(*wagon_ids)
+      wagon_ids
+    end
 
-  def self.fix_list_of_wagons(wagons_id, missing_wagons)
-    wagons_id.rotate!(2)
-    wagons_id.insert(1, *missing_wagons)
-  end
+    def fix_list_of_wagons(wagons_id, missing_wagons)
+      wagons_id.rotate!(2)
+      wagons_id.insert(1, *missing_wagons)
+    end
 
-  def self.add_missing_stops(route, **stops)
-    route.merge(stops: stops.values)
-  end
+    def add_missing_stops(route, **stops)
+      route.merge(stops: stops.values)
+    end
 
-  def self.extend_route_information(route, more_route_information)
-    { **route, **more_route_information }
+    def extend_route_information(route, more_route_information)
+      { **route, **more_route_information }
+    end
   end
 end
