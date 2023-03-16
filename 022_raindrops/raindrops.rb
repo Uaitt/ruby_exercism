8c8fc47a53efee913250ec4b1915890fa7dda560
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-03-16 09:29:03 +0100

    perf: make class method faster

--- 022_raindrops/raindrops.rb
+++ 022_raindrops/raindrops.rb
@@ -2,24 +2,11 @@ class Raindrops
   FACTORS = [3, 5, 7].freeze
 
   class << self
-    def convert(number)
-      raindrops = ''
-      calculate_raindrops(number, raindrops)
-      raindrops
-    end
-
-    private
-
-    def calculate_raindrops(number, raindrops)
+    def convert(number, raindrops = '')
       raindrops << 'Pling' if (number % FACTORS[0]).zero?
-
       raindrops << 'Plang' if (number % FACTORS[1]).zero?
-
       raindrops << 'Plong' if (number % FACTORS[2]).zero?
-
-      raindrops << number.to_s if !(number % FACTORS[0]).zero? &&
-                                  !(number % FACTORS[1]).zero? &&
-                                  !(number % FACTORS[2]).zero?
+      raindrops.empty? ? number.to_s : raindrops
     end
   end
 end
