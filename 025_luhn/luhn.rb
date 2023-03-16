a3507707bd1912a8fab71e62b33a434ff438ec15
Lorenzo Zabot <lorenzozabot@gmail.com>
2023-03-16 10:54:49 +0100

    feat: add 25th exercise

---
+++ 025_luhn/luhn.rb
@@ -0,0 +1,25 @@
+class Luhn
+  class << self
+    def valid?(input)
+      input.gsub!(/\s/, '')
+      return false if input.length <= 1 || !input.scan(/\D/).empty?
+
+      double_second_digits(input)
+      (input.split('').map(&:to_i).sum % 10).zero?
+    end
+
+    private
+
+    def double_second_digits(input)
+      index = -2
+      until input[index].nil?
+        input[index] = if input[index].to_i * 2 > 9
+                         (input[index].to_i * 2 - 9).to_s
+                       else
+                         (input[index].to_i * 2).to_s
+                       end
+        index -= 2
+      end
+    end
+  end
+end
